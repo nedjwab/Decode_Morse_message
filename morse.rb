8d0581817bb9433718e0d7054c46e861402ec62c
ilham bouaik <ibouaik@gmail.com>
2022-07-27 12:28:32 +0100

    create a method to decode a entrie word and message.

--- morse.rb
+++ morse.rb
@@ -8,6 +8,30 @@ def decode_char(char)
     'q' => '--.-', 'r' => '.-.', 's' => '...', 't' => '-', 'u' => '..-', 'v' => '...-',
     'w' => '.--', 'x' => '-..-', 'y' => '-.--', 'z' => '--..'
   }
-  puts morse_signs.key(char).upcase
+  morse_signs.key(char).upcase
 end
-decode_char('.-')
+
+decode_char('-..-')
+
+def decode_word(morse_word)
+  word = ''
+  chars = morse_word.split(' ')
+  chars.each do |char|
+    word += decode_char(char)
+  end
+  word
+end
+
+puts decode_word('-- -.--')
+
+def decode_phrase(morse_phrase)
+  phrase = []
+  words = morse_phrase.split('   ')
+  words.each do |word|
+    phrase.push(decode_word(word))
+  end
+
+  phrase.join(' ')
+end
+
+puts decode_phrase('-- -.--   -. .- -- .')
